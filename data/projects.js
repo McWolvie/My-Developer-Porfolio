fcb173b2d47414f28e39c1ea88b30484db6b87f1
McWolvie <lobezno2001@gmail.com>
2023-07-24 14:06:08 +0200

    My Developer Portfolio

---
+++ data/projects.js
@@ -0,0 +1,98 @@
+export const projects = [
+    {
+    title: "First project",
+    description: "Learn HTML by Building a Cat Photo App",
+    image:
+    "http://127.0.0.1:5500/MyFreeCodeCamp/Cat_Photo_App/index.html",
+    tech: ["HTML5"],
+    github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Cat_Photo_App",
+    link: "www.Geek-Planet.com",
+    },
+    {
+    title: "Second project",
+    description: "Learn Basic CSS by Building a Cafe Menu",
+    image:
+    "http://127.0.0.1:5500/MyFreeCodeCamp/Camper_Coffe/index.html",
+    tech: ["HTML5", "CSS3"],
+    github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Camper_Coffe",
+    link: "www.Geek-Planet.com",
+    },
+    {
+    title: "Third project",
+    description: "Learn CSS Colors by Building a Set of Colored Markers",
+    image:
+    "http://127.0.0.1:5500/MyFreeCodeCamp/Color_Markers/index.html",
+    tech: ["HTML5", "CSS3"],
+    github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Color_Markers",
+    link: "www.Geek-Planet.com",
+    },
+    {
+    title: "Fourth project",
+    description: "Learn HTML Forms by Building a Registration Form",
+    image:
+    "http://127.0.0.1:5500/MyFreeCodeCamp/Registration_Form/index.html",
+    tech: ["HTML5", "CSS3"],
+    github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Registration_Form",
+    link: "www.Geek-Planet.com",
+    },
+    {
+    title: "Fifth project",
+    description: "Replica of the website of the band Sleep Token",
+    image:
+    "http://127.0.0.1:5500/Sleep_Token_Replicate/index.html",
+    tech: ["HTML5", "CSS3", "JavaScript"],
+    github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Registration_Form",
+    link: "www.Geek-Planet.com",
+    },
+
+    {
+        title: "Fifth project",
+        description: "Replica of the website of the band Sleep Token",
+        image:
+        "http://127.0.0.1:5500/Sleep_Token_Replicate/index.html",
+        tech: ["HTML5", "CSS3", "JavaScript"],
+        github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Registration_Form",
+        link: "www.Geek-Planet.com",
+        },
+
+     {
+         title: "Sixth project",
+         description: "Replica of the website of the band Sleep Token",
+         image:
+        "http://127.0.0.1:5500/Sleep_Token_Replicate/index.html",
+        tech: ["HTML5", "CSS3", "JavaScript"],
+        github: "https://github.com/McWolvie/Sleep_Token_Replicate",
+        link: "www.Geek-Planet.com",
+        },
+
+     {
+        title: "Seventh project",
+        description: "Color Flipper, an application that will allow us to change the color of our background",
+        image:
+        "https://vercel.com/mcwolvie/project-color-flipper",
+        tech: ["HTML5", "CSS3", "JavaScript"],
+        github: "https://github.com/McWolvie/Project-Color-Flipper",
+        link: "www.Geek-Planet.com",
+        },
+     
+        {
+            title: "Eighth project",
+            description: "project game: Rock, Paper, Scissors, Lizard, Spock",
+            image:
+            "https://vercel.com/mcwolvie/project-game-vite",
+            tech: ["HTML5", "CSS3", "JavaScript"],
+            github: "https://github.com/McWolvie/Project-Game-Vite",
+            link: "www.Geek-Planet.com",
+            },
+      
+        {
+            title: "Ninth project",
+                description: "weather app project",
+                image:
+                "https://vercel.com/mcwolvie/project-app-weather-forecast",
+                tech: ["HTML5", "CSS3", "JavaScript"],
+                github: "https://github.com/McWolvie/Project-App-Weather-Forecast",
+                link: "www.Geek-Planet.com",
+                },
+        
+    ]
