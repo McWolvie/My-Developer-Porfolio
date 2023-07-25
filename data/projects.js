7ddb177cd7d67f1ed5f9af9800ea5bda798f0512
McWolvie <lobezno2001@gmail.com>
2023-07-25 14:15:30 +0200

    My Developer Portfolio

--- data/projects.js
+++ data/projects.js
@@ -3,96 +3,87 @@ export const projects = [
     title: "First project",
     description: "Learn HTML by Building a Cat Photo App",
     image:
-    "http://127.0.0.1:5500/MyFreeCodeCamp/Cat_Photo_App/index.html",
+    "/public/img/CatPhotoApp.jpg",
     tech: ["HTML5"],
     github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Cat_Photo_App",
-    link: "www.Geek-Planet.com",
+    link: "https://www.youtube.com/",
     },
     {
     title: "Second project",
     description: "Learn Basic CSS by Building a Cafe Menu",
     image:
-    "http://127.0.0.1:5500/MyFreeCodeCamp/Camper_Coffe/index.html",
+    "/public/img/CamperCoffee.jpg",
     tech: ["HTML5", "CSS3"],
     github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Camper_Coffe",
-    link: "www.Geek-Planet.com",
+    link: "https://www.youtube.com/",
     },
     {
     title: "Third project",
     description: "Learn CSS Colors by Building a Set of Colored Markers",
     image:
-    "http://127.0.0.1:5500/MyFreeCodeCamp/Color_Markers/index.html",
+    "/public/img/ColorsMakers.jpg",
     tech: ["HTML5", "CSS3"],
     github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Color_Markers",
-    link: "www.Geek-Planet.com",
+    link: "https://www.youtube.com/",
     },
     {
     title: "Fourth project",
     description: "Learn HTML Forms by Building a Registration Form",
     image:
-    "http://127.0.0.1:5500/MyFreeCodeCamp/Registration_Form/index.html",
+    "/public/img/RegistrationForm.jpg",
     tech: ["HTML5", "CSS3"],
     github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Registration_Form",
-    link: "www.Geek-Planet.com",
+    link: "https://www.youtube.com/",
     },
     {
     title: "Fifth project",
     description: "Replica of the website of the band Sleep Token",
     image:
-    "http://127.0.0.1:5500/Sleep_Token_Replicate/index.html",
+    "/public/img/SleepToken.jpg",
     tech: ["HTML5", "CSS3", "JavaScript"],
     github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Registration_Form",
-    link: "www.Geek-Planet.com",
+    link: "https://www.youtube.com/",
     },
-
-    {
-        title: "Fifth project",
-        description: "Replica of the website of the band Sleep Token",
-        image:
-        "http://127.0.0.1:5500/Sleep_Token_Replicate/index.html",
-        tech: ["HTML5", "CSS3", "JavaScript"],
-        github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Registration_Form",
-        link: "www.Geek-Planet.com",
-        },
-
-     {
-         title: "Sixth project",
-         description: "Replica of the website of the band Sleep Token",
-         image:
-        "http://127.0.0.1:5500/Sleep_Token_Replicate/index.html",
-        tech: ["HTML5", "CSS3", "JavaScript"],
-        github: "https://github.com/McWolvie/Sleep_Token_Replicate",
-        link: "www.Geek-Planet.com",
-        },
 
      {
         title: "Seventh project",
         description: "Color Flipper, an application that will allow us to change the color of our background",
         image:
-        "https://vercel.com/mcwolvie/project-color-flipper",
+        "/public/img/ColorFlipper.jpg",
         tech: ["HTML5", "CSS3", "JavaScript"],
         github: "https://github.com/McWolvie/Project-Color-Flipper",
-        link: "www.Geek-Planet.com",
+        link: "https://www.youtube.com/",
         },
      
         {
             title: "Eighth project",
             description: "project game: Rock, Paper, Scissors, Lizard, Spock",
             image:
-            "https://vercel.com/mcwolvie/project-game-vite",
+            "/public/img/GameRockPaperScissorsLizardSpock.jpg",
             tech: ["HTML5", "CSS3", "JavaScript"],
             github: "https://github.com/McWolvie/Project-Game-Vite",
-            link: "www.Geek-Planet.com",
+            link: "https://www.youtube.com/",
             },
       
         {
             title: "Ninth project",
                 description: "weather app project",
                 image:
-                "https://vercel.com/mcwolvie/project-app-weather-forecast",
+                "/public/img/WeatherForecast.jpg",
                 tech: ["HTML5", "CSS3", "JavaScript"],
                 github: "https://github.com/McWolvie/Project-App-Weather-Forecast",
-                link: "www.Geek-Planet.com",
+                link: "https://www.youtube.com/",
                 },
+
+                {
+                    title: "Tenth project",
+                        description: "Portfolio app project",
+                        image:
+                        "/public/img/PortfolioProject.jpg",
+                        tech: ["HTML5", "CSS3", "JavaScript"],
+                        github: "https://github.com/McWolvie/Project-Portfolio-Vite",
+                        link: "https://www.youtube.com/",
+                        },
+        
         
     ]
---
+++ data/skills.js
@@ -0,0 +1,47 @@
+export const skills = [
+    {
+        title: "First project",
+        description: "Learn HTML by Building a Cat Photo App",
+        image:
+        "/public/img/CatPhotoApp.jpg",
+        tech: ["HTML5"],
+        github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Cat_Photo_App",
+        link: "https://www.youtube.com/",
+        },
+        {
+        title: "Second project",
+        description: "Learn Basic CSS by Building a Cafe Menu",
+        image:
+        "/public/img/CamperCoffee.jpg",
+        tech: ["HTML5", "CSS3"],
+        github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Camper_Coffe",
+        link: "https://www.youtube.com/",
+        },
+        {
+        title: "Third project",
+        description: "Learn CSS Colors by Building a Set of Colored Markers",
+        image:
+        "/public/img/ColorsMakers.jpg",
+        tech: ["HTML5", "CSS3"],
+        github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Color_Markers",
+        link: "https://www.youtube.com/",
+        },
+        {
+        title: "Fourth project",
+        description: "Learn HTML Forms by Building a Registration Form",
+        image:
+        "/public/img/RegistrationForm.jpg",
+        tech: ["HTML5", "CSS3"],
+        github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Registration_Form",
+        link: "https://www.youtube.com/",
+        },
+        {
+        title: "Fifth project",
+        description: "Replica of the website of the band Sleep Token",
+        image:
+        "/public/img/SleepToken.jpg",
+        tech: ["HTML5", "CSS3", "JavaScript"],
+        github: "https://github.com/McWolvie/MyFreeCodeCamp/tree/main/Registration_Form",
+        link: "https://www.youtube.com/",
+        },
+    ]
